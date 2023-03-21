300d6572fea0823cde87efc364a27c2fe98ab001
IdaDanuartha <idadanuartha42@smk.belajar.id>
2023-03-21 22:20:34 +0800

    variasi pola segitiga

--- 06-Pola Segitiga/main.cpp
+++ 06-Pola Segitiga/main.cpp
@@ -3,15 +3,86 @@ using namespace std;
 
 int main()
 {
-    int limit;
-    cout << "Ukuran : "; cin >> limit;
+    int size;
+    cout << "Ukuran : "; cin >> size;
 
-    for(int i = 1; i <= limit; i++)
+    // Pola Segitiga Siku-Siku
+    
+    // for(int i = 1; i <= size; i++)
+    // {
+    //     for (int j = 1; j < i; j++)
+    //     {
+    //         cout << "o ";
+    //     }
+    //     cout << "o" << endl;
+    // }
+
+    // Pola segitiga tengah bolong
+    
+    // for(int i = 1; i <= size; i++) {
+    //     for (int j = 1; j < i; j++)
+    //         if(j == 1 || i == size) {
+    //             cout << "o ";
+    //         } else {
+    //             cout << "  ";
+    //         }
+    //     }
+    //             cout << "o ";
+    //     cout << "o" << endl;
+    // }
+
+    // Pola segitiga sama kaki
+    
+    // for(int i = 1; i <= size; i++)
+    // {
+    //     for (int j = size; j > i; j--)
+    //     {
+    //         cout << " ";
+    //     }
+    //     for(int k = 1; k <= (2*i-1); k++)
+    //     {
+    //         cout << "o";
+    //     }
+    //     cout << endl;
+    // }
+
+    // Pola segitiga sama kaki terbalik
+    // for(int i = 1; i <= size; i++)
+    // {
+    //     for (int j = 1; j < i; j++)
+    //     {
+    //         cout << " ";
+    //     }
+    //     for(int k = size; k >= (2*i-size); k--)
+    //     {
+    //         cout << "o";
+    //     }
+    //     cout << endl;
+    // }
+
+    // Pola belah ketupat
+    for(int i = 1; i <= size; i++)
+    {
+        for (int j = size; j > i; j--)
+        {
+            cout << " ";
+        }
+        for(int k = 1; k <= (2*i-1); k++)
+        {
+            cout << "o";
+        }
+        cout << endl;
+    }
+    for(int i = 1; i <= size; i++)
     {
         for (int j = 1; j < i; j++)
         {
-            cout << "* ";
+            cout << " ";
         }
-        cout << "*" << endl;
+        for(int k = size; k >= (2*i-size); k--)
+        {
+            cout << "o";
+        }
+        cout << endl;
     }
 }
